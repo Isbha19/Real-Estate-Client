081488fa5f47f2a9ace880ebd82f3af68d0ba226
Isbha Khalid <isbhakhalid19@gmail.com>
2024-08-02 10:02:14 +0400

    added dynamic listing of subscription packages

--- src/app/components/Company/pages/subscription-packages/subscription-packages.component.ts
+++ src/app/components/Company/pages/subscription-packages/subscription-packages.component.ts
@@ -1,21 +1,26 @@
+import { SubscriptionPackageService } from './../../../Subscriptions/services/subscriptionPackage.service';
 import { AccountService } from './../../../../core/service/account.service';
 import { Component } from '@angular/core';
 import { JwtDecodedToken } from '../../../../core/model/jwtTokenDecoded';
 import { jwtDecode } from 'jwt-decode';
 import { CompanyService } from '../../service/company.service';
+import { Package } from '../../../Subscriptions/model/package';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-subscription-packages',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './subscription-packages.component.html',
   styleUrl: './subscription-packages.component.scss'
 })
 export class SubscriptionPackagesComponent {
  constructor(private accountService:AccountService,
-  private companyService:CompanyService
+  private companyService:CompanyService,
+  private subscriptionPackageService:SubscriptionPackageService
  ){}
 userEmail:string="";
+packages: Package[] = [];
 
 ngOnInit(): void {
   this.accountService.user$.subscribe((user) => {
@@ -24,6 +29,10 @@ ngOnInit(): void {
       this.userEmail=decodedToken.email;
     }
   });
+
+  this.subscriptionPackageService.getPackages().subscribe((data: Package[])=>{
+this.packages=data;
+  })
 }
 validateAndProceedToPayment(paymentUrl: string) {
 
